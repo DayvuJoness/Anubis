f79889a1131b4406b742d66808f706cf6bd6046e
Dominig12 <daliskandarov.ru@mail.ru>
2020-05-22 13:59:33 +0300

    ref

--- Assets/Scripts/DieSpace.cs
+++ Assets/Scripts/DieSpace.cs
@@ -7,29 +7,4 @@ public class DieSpace : MonoBehaviour
 {
     int Life = 10;
     public GameObject respawn;
-    /*void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag=="Player")
-        {
-            Life = Life - 1;
-            if (Life <= 0)
-            {
-                Invoke("Respawn", 2);
-            }
-            //System.Threading.Thread.Sleep(500);
-            //yield return new WaitForSeconds(0.1f);
-            //other.transform.position = respawn.transform.position;
-            
-        }
-    }
-    void OnGUI()
-    {
-        GUI.Box(new Rect(0, 0, Screen.width/7, Screen.height/15), "Life = " + Life);
-    }
-    void Respawn()
-    {
-        Application.LoadLevel(Application.loadedLevel);
-        //other.transform.position = respawn.transform.position;
-    }*/
-
 }
